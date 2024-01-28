5a654ea2bc96d087cbdd23ab5429d06c0b9690ea
wjhuang98 <williamhuang988@gmail.com>
2024-01-28 18:14:06 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x):
+        return x
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +26,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
+
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -27,6 +36,9 @@ class Sub:
 
     def __repr__(self):
         return repr(self.p1) + " - " + repr(self.p2)
+
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -45,6 +57,9 @@ class Mul:
             repr_p2 = repr(self.p2)
 
         return repr_p1 + " * " + repr_p2
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
 
 class Div:
     def __init__(self, p1, p2):
@@ -64,6 +79,9 @@ class Div:
 
         return repr_p1 + " / " + repr_p2
 
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
+
 poly_div = Div(Add(Int(10), X()), Int(2))
 poly_sub = Sub(Add(X(), Int(5)), Mul(X(), Int(2)))
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
@@ -72,3 +90,6 @@ poly = Add(poly, Div(Int(8), Sub(X(), Int(2))))
 print(poly_div)
 print(poly_sub)
 print(poly)
+
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
